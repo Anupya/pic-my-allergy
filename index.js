ee49cce72fe397ef5417034bd10b41617a423c68
Anupya Pamidimukkala <anupya@hotmail.ca>
2018-03-09 12:20:12 -0500

    uploads image

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* define our application and instantiate Express */
-var app = require('express')();
+var express = require('express');
+var app = express();
 
 /* pass our app into http */
 var http = require('http').Server(app);
@@ -10,12 +11,15 @@ var hbs = require('hbs');
 /* define a route to handle form post */
 var path = require('path');
 
-var bodyParser = require('body-parser');
-
 /* store responses using FileSystem */
 var fs = require('fs');
 
-var multer = require('multer');
+var formidable = require('formidable');
+var rimraf = require('rimraf');
+var FileAPI = require('file-api');
+var File = FileAPI.file;
+var FileList = FileAPI.FileList;
+var FileReader = FileAPI.FileReader;
 
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
@@ -59,37 +63,87 @@ app.get('/reset', function(req, res) {
 })
 
 /* upload */
-/* req is undefined */
-app.get('/upload', function (req, res) {
-	console.log("IN UPLOAD");
-	console.log("REQ: " + req.file);
-
-    var tempPath = req.file.path,
-        targetPath = path.resolve('./uploads/image.png');
-    if (path.extname(req.file.name).toLowerCase() === '.png') {
-        upload(req, res, function(err) {
-        	if (err) {
-        		return res.end("Error uploading files.");
-        	}
-        	res.end("File is uploaded");
-        });
-    } else {
-        fs.unlink(tempPath, function () {
-            if (err) throw err;
-            console.error("Only .png files are allowed!");
-        });
-    }
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.use(bodyParser({uploadDir:'./uploads'}));
+/*
+$("#the-file-input").change(function() {
+	console.log("photo file has been chosen");
+	//grab the first image in the fileList
+	//in this example we are only loading one file.
+	console.log(this.files[0].size);
+	console.log(this.files[0]);
+	renderImage(this.files[0]);
+});
+*/
 
-/* display the image */
-app.get('/image.png', function(req, res) {
+/* req is undefined */
+app.post('/upload', function(req, res){
+	console.log("IN UPLOADS");
+	// clear the uploads directory
+	rimraf('./uploads/*', function() {});
 
-	res.sendFile(path.resolve('./uploads/image.png'));
-	res.redirect('/');
-})
+    // create an incoming form object
+    var form = new formidable.IncomingForm();
 
+    // specify that we want to allow the user to upload multiple files in a single request
+    // form.multiples = true;
+
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '/uploads');
+
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function(field, file) {
+
+  	  if ((file.name.split('.').pop() == 'jpg') || 
+  		  (file.name.split('.').pop() == 'png')) {
+  			  var ext = "png";
+  			  fs.rename(file.path, path.join(form.uploadDir, ("image." + ext)));
+  	  }
+  	  else {
+  		  res.end("Sorry, we only accept JPGs and PNGs");
+  	  }
+    });
+
+    // log any errors that occur
+    form.on('error', function(err) {
+      console.log('An error has occurred: \n' + err);
+    });
+
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function() {
+    	console.log("SUCCESS");
+    	// renderImage(req.file);
+    });
+
+    // parse the incoming request containing the form data
+    form.parse(req);
+    console.log("ABOUT TO DISPLAY IMAGE FILE ");
+
+    // display image file to browser 
+    //renderImage('./uploads/image.png');
+    //console.log("RENDER IMAGE HAS RUN");
+    //res.redirect('/');
+});
+
+function renderImage (file) {
+	var reader = new FileReader();
+
+	reader.onload = function(event) {
+		the_url = event.target.result;
+		console.log("the_url: " + the_url);
+		$('#frmUploader').html("<img src='" + the_url + "' />");
+	}
+
+	reader.readAsDataURL(file);
+
+	$('#the-file-input').change(function() {
+		console.log(this.files);
+		renderImage(this.files[0]);
+	});
+}
 
 /* host the website */
 http.listen(3000, function() {
